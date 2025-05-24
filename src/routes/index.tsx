a496d125a8761736eedbbed39c624775aae1d840
Aljaz Gracner <aljaz.gracner@dhimahi.com>
2025-05-24 18:45:37 +0200

    p

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,18 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useRouter } from '@tanstack/react-router'
 import { Toaster, toast } from 'sonner'
+import { useEffect } from "react";
 
 export const Route = createFileRoute('/')({
   component: App,
 })
 
 function App() {
+    const router = useRouter();
+
+    useEffect(() => {
+        router.navigate({ to: "/analytics" });
+    }, [router]);
+
   return (
     <>
       <Toaster />
